91acc8248049d20a928874d1813176851611e25a
Jason Schorr <echohtp@gmail.com>
2022-10-20 17:33:16 -0400

    port 443

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(3000, (err) => {
+  }).listen(443, (err) => {
     if (err) throw err;
     console.log("> Server started on https://localhost:3000");
   });
