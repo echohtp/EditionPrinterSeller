0e203ff7754ad68fb52b8d550c6491beafe82793
Jason Schorr <echohtp@gmail.com>
2022-10-20 17:32:09 -0400

    https?

---
+++ server.js
@@ -0,0 +1,20 @@
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/techdebt.0xbanana.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/techdebt.0xbanana.com/fullchain.pem"),
+};
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(3000, (err) => {
+    if (err) throw err;
+    console.log("> Server started on https://localhost:3000");
+  });
+});
