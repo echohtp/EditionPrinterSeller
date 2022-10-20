9378129b7df64416aa84f0bc5002f258f6a5d7a9
Jason Schorr <echohtp@gmail.com>
2022-10-20 17:22:38 -0400

    adjust

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,7 +34,7 @@ function MyApp ({ Component, pageProps }: AppProps) {
   const network = WalletAdapterNetwork.Mainnet
 
   // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => process.env.NEXT_PUBLIC_RPC, [network])
+  const endpoint = useMemo(() => process.env.NEXT_PUBLIC_RPC!, [network])
 
   // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
   // Only the wallets you configure here will be compiled into your application, and only the dependencies
