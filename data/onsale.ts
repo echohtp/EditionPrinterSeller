43968c5de17709e5489681699bfc98c1d21153cf
Jason Schorr <echohtp@gmail.com>
2022-10-21 15:39:34 -0400

    use correctm int

--- data/onsale.ts
+++ data/onsale.ts
@@ -7,7 +7,7 @@ const mintsOnSale = [
     bankAta: '437mKN7wtYVkH8cUgDR4Lyi4bfUyWnChvh62Nk6y8yix',
     price: 75,
     symbol: 'BNON',
-    mint: 'GLE8YHQ2DFPgBPH977VRTiXs5A2vaQoAAEvPqsVmKNWx',
+    mint: 'BvCAdmPA8xuz1Cx3H3Fo6fF8GHji2Evwv6DB1X9szicj',
     image: 'https://assets1.holaplex.tools/ipfs/bafybeiatemwoesf43eqzdieojhrdxl6pgbdhnpi2llkzmq3hn3n2tibh24'
   },
   {
@@ -16,7 +16,7 @@ const mintsOnSale = [
     bankAta: '',
     price: 1.69,
     symbol: 'SOL',
-    mint: 'GLE8YHQ2DFPgBPH977VRTiXs5A2vaQoAAEvPqsVmKNWx',
+    mint: 'BvCAdmPA8xuz1Cx3H3Fo6fF8GHji2Evwv6DB1X9szicj',
     image: 'https://assets1.holaplex.tools/ipfs/bafybeiatemwoesf43eqzdieojhrdxl6pgbdhnpi2llkzmq3hn3n2tibh24'
   }
 ]
