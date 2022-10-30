09e312edbcea5ff919868a5ce884d0161b97db0c
Jason Schorr <echohtp@gmail.com>
2022-10-30 18:18:24 -0400

    just slamming shit into main

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const httpsOptions = {
-  key: fs.readFileSync("/etc/letsencrypt/live/techdebt.0xbanana.com/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/techdebt.0xbanana.com/fullchain.pem"),
+  key: fs.readFileSync("/etc/letsencrypt/live/editions.0xbanana.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/editions.0xbanana.com/fullchain.pem"),
 };
 app.prepare().then(() => {
   createServer(httpsOptions, (req, res) => {
@@ -15,6 +15,6 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl);
   }).listen(443, (err) => {
     if (err) throw err;
-    console.log("> Server started on https://localhost:3000");
+    console.log("> Server started on https://localhost");
   });
 });
